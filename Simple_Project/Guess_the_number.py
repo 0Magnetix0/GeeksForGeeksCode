e7fe76d3a6fe3ae4365a24f142a223768e1739c1
OMagnetixO <sudhanshujoshi@029@gmail.com>
2020-03-01 15:10:28 +0530

    removing previous code

--- Simple_Project/Guess_the_number.py
+++ Simple_Project/Guess_the_number.py
@@ -1,1 +1,14 @@
-n = 
+def guess_the_no():
+    print("Guess the number from 1 to 20")
+    guess = int(input())
+    if guess == n:
+        print("Correct answer")
+    elif guess > n:
+        print("To high")
+        guess_the_no()
+    else:
+        print("To low")
+        guess_the_no()
+
+n = 14
+guess_the_no()
--- hello.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-  printf("just a file\n");
-  return 0;
-}

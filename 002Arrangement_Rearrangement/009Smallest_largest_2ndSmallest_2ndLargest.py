865f6b70e8bb736d758b53d6c24053f134f58a36
OMagnetixO <sudhanshujoshi@029@gmail.com>
2020-03-16 13:20:45 +0530

    one more addition

---
+++ 002Arrangement_Rearrangement/009Smallest_largest_2ndSmallest_2ndLargest.py
@@ -0,0 +1,18 @@
+#Sort the array in such a way that the element of the odd position is lower that its even counter parts.
+for _ in range(int(input())):
+    n = int(input())
+    l = list(map(int,input().split()))
+    cl = [x for x in l]
+    cl.sort()
+    j = 0
+    k = len(cl) - 1
+    for i in range(len(l)):
+        if (i+1) % 2 == 0:
+            l[i] = cl[k]
+            k -= 1
+        else:
+            l[i] = cl[j]
+            j += 1
+    for i in range(len(l)):
+        print(l[i], end = " ")
+    print()
